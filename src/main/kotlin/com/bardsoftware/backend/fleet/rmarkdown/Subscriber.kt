7011e591615b4c34cb7c52918bb262fc6384a725
shavkunov <mv.shavkunov@yandex.ru>
2018-03-27 19:58:32 +0300

    refactored subsriber

--- src/main/kotlin/com/bardsoftware/backend/fleet/rmarkdown/Subscriber.kt
+++ src/main/kotlin/com/bardsoftware/backend/fleet/rmarkdown/Subscriber.kt
@@ -7,11 +7,16 @@ import com.google.cloud.pubsub.v1.Subscriber
 import com.google.pubsub.v1.PubsubMessage
 import com.google.pubsub.v1.SubscriptionName
 import java.util.concurrent.LinkedBlockingDeque
+import com.xenomachina.argparser.ArgParser
+
+class SubscriberArgs(parser: ArgParser) {
+    val subscriberName by parser.storing(
+            "--sub",
+            help = "subscription topic name")
+}
 
 object SubscriberExample {
-    private const val subscriberName = "rmarkdown-compiler"
 
-    // use the default project id
     private val PROJECT_ID = ServiceOptions.getDefaultProjectId()
 
     private val messages = LinkedBlockingDeque<PubsubMessage>()
@@ -24,19 +29,15 @@ object SubscriberExample {
         }
     }
 
-    /** Receive messages over a subscription.  */
-    @Throws(Exception::class)
-    @JvmStatic
     fun main(args: Array<String>) {
-        // set subscriber id, eg. my-sub
-        val subscriptionId = subscriberName
+        val parsedArgs = ArgParser(args).parseInto(::SubscriberArgs)
+        val subscriptionId = parsedArgs.subscriberName
         val subscriptionName = SubscriptionName.of(PROJECT_ID, subscriptionId)
+
         var subscriber: Subscriber? = null
         try {
-            // create a subscriber bound to the asynchronous message receiver
             subscriber = Subscriber.newBuilder(subscriptionName, MessageReceiverExample()).build()
-            subscriber!!.startAsync().awaitRunning()
-            // Continue to listen to messages
+            subscriber.startAsync().awaitRunning()
             while (true) {
                 val message = messages.take()
                 println("Message Id: " + message.messageId)
